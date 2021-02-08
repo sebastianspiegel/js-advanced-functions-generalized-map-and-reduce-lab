e3b0c67c4a10fbd9f89cef268b6600afeceab5f9
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-02-08 12:48:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map(srcArr, fun){
+    let newArray = []
+    for (let i = 0; i < srcArr.length; i++){
+        newArray.push(fun(srcArr[i]))
+    }
+    return newArray
+}
+
+function reduce(srcArr, fun, startingPoint){
+    let ans = (!!startingPoint) ? startingPoint : srcArr[0]
+    for (let i = (!!startingPoint) ? 0 : 1; i < srcArr.length; i++){
+        ans = fun(srcArr[i], ans)
+    }
+    return ans  
+}
+
